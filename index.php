d95b648946b6aa1d96053b020f6bad96e6ef9c59
kujirahand <web@kujirahand.com>
2024-12-27 21:18:55 +0900

    change url

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://nadesiko.net/index.php?nako3kvs";
+$url = "https://nadesi.com/v3/doc/go.php?4637";
 header("location: $url");
 echo "<html><body>";
 echo "<a href='$url'>説明ページ</a>";
